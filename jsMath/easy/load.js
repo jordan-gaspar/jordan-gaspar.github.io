4b96072805cd90144c462e9048316398bb405016
jordan-gaspar <212644380+jordan-gaspar@users.noreply.github.com>
2025-05-31 22:06:29 -0500

    Update load.js

--- jsMath/easy/load.js
+++ jsMath/easy/load.js
@@ -53,9 +53,9 @@ jsMath.Easy = {
   processSlashParens: 1,       // process \(...\) in text?
   processSlashBrackets: 1,     // process \[...\] in text?
   processDoubleDollars: 1,     // process $$...$$ in text?
-  processSingleDollars: 0,     // process $...$ in text?
+  processSingleDollars: 1,     // process $...$ in text?
   processLaTeXenvironments: 0, // process \begin{xxx}...\end{xxx} outside math mode?
-  fixEscapedDollars: 0,        // convert \$ to $ outside of math mode?
+  fixEscapedDollars: 1,        // convert \$ to $ outside of math mode?
   doubleDollarsAreInLine: 0,   // make $$...$$ be in-line math?
   allowDisableTag: 1,          // allow ID="tex2math_off" to disable tex2math?
   //
